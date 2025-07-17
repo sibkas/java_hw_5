2585aefec469983f41ed25857ea6a6563641135c
akovr <akovr@yahoo.com>
2025-07-17 12:41:44 +0700

    update project

--- src/main/java/ru/netology/hw5/services/FreelancerService.java
+++ src/main/java/ru/netology/hw5/services/FreelancerService.java
@@ -1,4 +1,27 @@
-package ru.netology.hw5;
+package ru.netology.hw5.services;
 
 public class FreelancerService {
+
+     //income - доход за месяц работы
+     //expense - обязательные расходы
+     //threshold - минимальная сумма на счету, чтобы можно было отдохнуть
+     //@return количество месяцев отдыха за год (12 мес)
+
+    public int calculate(int income, int expense, int threshold) {
+        int count = 0; // месяцев отдыха
+        int money = 0; // денег на счету
+
+        for (int month = 1; month <= 12; month++) {
+            if (money >= threshold) {
+                // Можем отдыхать
+                count++; // Отдыхаем в этом месяце
+                money = money - expense; // Тратим расходы на отдых
+                money = money / 3; // Ещё тратим 1/3 остатка
+            } else {
+                // Работаем
+                money = money + income - expense;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/ru/netology/hw5/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/hw5/services/FreelancerServiceTest.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+package ru.netology.hw5.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FreelancerServiceTest {
+
+    @ParameterizedTest
+//    @CsvSource({
+//            "10_000, 3_000, 20_000"
+//    })
+    @CsvFileSource (files="src/test/resources/money.csv")
+    public void testExample1(int income, int expense, int threshold, int expected) {
+        FreelancerService service = new FreelancerService();
+        //int income = 10_000;
+        //int expense = 3_000;
+        //int threshold = 20_000;
+        //int expected = 3; // согласно примеру
+        int actual = service.calculate(income, expense, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test
+//    public void testExample1() {
+//        FreelancerService service = new FreelancerService();
+//        int income = 10_000;
+//        int expense = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3; // согласно примеру
+//        int actual = service.calculate(income, expense, threshold);
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void testExample2() {
+//        FreelancerService service = new FreelancerService();
+//        int income = 100_000;
+//        int expense = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2; // согласно примеру
+//        int actual = service.calculate(income, expense, threshold);
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
